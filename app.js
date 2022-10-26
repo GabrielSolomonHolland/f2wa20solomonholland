fe1453eadb082177cf0f903f7720d74e0cac336e
GabrielSolomonHolland <69911029+GabrielSolomonHolland@users.noreply.github.com>
2022-10-25 19:57:49 -0500

    new endpoing

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var myDataRouter = require('./routes/myData')
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -19,7 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/myData', myDataRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
